ab02062b3cd98a95fe4f5894759eb4756edf28c5
Maoirt <122370101+Maoirt@users.noreply.github.com>
2024-04-24 09:49:22 +0300

    Create Person.py

---
+++ Person.py
@@ -0,0 +1,15 @@
+class Person:
+    count = 0
+    def __init__(self,name):
+        self.name = name
+        Person.count=Person.count+1
+    
+    @staticmethod
+    def total_people():
+        return f"Количество людей: {Person.count}"
+
+print(Person.total_people())        
+p = Person("Jhon")
+print(Person.total_people())
+p1 = Person("Dan")
+print(Person.total_people())
